9826db87ee56a4a5e2f2ff4bd6f93ee0513ee097
anubhav1701-git <anubhav.work1226@gmail.com>
2026-02-16 19:46:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
@@ -0,0 +1,59 @@
+class Solution {
+    
+    public void mergeArrays(int a[], int b[]) {
+        
+        int n = a.length;
+        int m = b.length;
+        
+        int gap = nextGap(n + m);
+        
+        while (gap > 0) {
+            
+            int i = 0;
+            int j = gap;
+            
+            while (j < n + m) {
+                
+               
+                if (i < n && j < n) {
+                    if (a[i] > a[j]) {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+                
+              
+                else if (i < n && j >= n) {
+                    if (a[i] > b[j - n]) {
+                        int temp = a[i];
+                        a[i] = b[j - n];
+                        b[j - n] = temp;
+                    }
+                }
+                
+          
+                else {
+                    if (b[i - n] > b[j - n]) {
+                        int temp = b[i - n];
+                        b[i - n] = b[j - n];
+                        b[j - n] = temp;
+                    }
+                }
+                
+                i++;
+                j++;
+            }
+            
+            gap = nextGap(gap);
+        }
+    }
+    
+    private int nextGap(int gap) {
+        if (gap <= 1)
+            return 0;
+        return (gap / 2) + (gap % 2);
+    }
+}
+
+
